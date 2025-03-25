9533bcb971bce4cb4af6a4834711bc653b6b9bbe
Folami <folami.bidmus@gmail.com>
2025-03-25 17:32:42 +0100

    [interpreter scanner functionality]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,50 @@
 import sys
-from py_lox import PyLox
+from scanner import Scanner
+
+class PyLox:
+    def __init__(self):
+        self.had_error = False
+        self.had_runtime_error = False
+
+
+    def run(self, source: str) -> None:
+        scanner = Scanner(source, self)
+        tokens = scanner.scan_tokens()
+
+        for token in tokens:
+            print(token)
+
+    def error(self, line: int, message: str) -> None:
+        self.report(line, "", message)
+        self.had_error = True
+
+    def report(self, line: int, where: str, message: str) -> None:
+        print(f"[line {line}] Error{where}: {message}", file=sys.stderr)
+        self.had_error = True
+
+    def runtime_error(self, error) -> None:
+        print(f"{error.message}\n[line {error.token.line}]", file=sys.stderr)
+        self.had_runtime_error = True
+
+
 
 def main():
     lox = PyLox()
-    if len(sys.argv) > 2:
-        print("Usage: pylox [script]", file=sys.stderr)
-        sys.exit(1)
-    elif len(sys.argv) == 2:
-        lox.run_file(sys.argv[1])
+    if len(sys.argv) < 3:
+        print("Usage: ./your_program.sh tokenize <filename>", file=sys.stderr)
+        exit(1)
+
+    command = sys.argv[1]
+    filename = sys.argv[2]
+
+    with open(filename) as file:
+        file_contents = file.read()
+
+    if command == "tokenize":
+        scanner = Scanner(file_contents, lox)
+        scanner.scan_tokens()
     else:
-        lox.run_prompt()
+        print("EOF  null") # Placeholder, remove this line when implementing the scanner
 
 if __name__ == "__main__":
     main()
