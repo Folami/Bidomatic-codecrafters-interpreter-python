45e7bea6a92c0a873b8cce1c5ab13e53c54420a3
Folami <folami.bidmus@gmail.com>
2025-04-26 19:14:07 +0100

    [Identifier Resolution]

--- app/lox_callable.py
+++ app/lox_callable.py
@@ -1,13 +1,15 @@
 from abc import ABC, abstractmethod
 from typing import Any, List
-from app.interpreter import Interpreter
+# from app.interpreter import Interpreter
 
 class LoxCallable(ABC):
     @abstractmethod
     def arity(self) -> int:
         """Return the number of arguments this callable accepts."""
+        from app.interpreter import Interpreter
         pass
 
     @abstractmethod
     def call(self, interpreter: Interpreter, arguments: List[Any]) -> Any:
+        from app.interpreter import Interpreter
         pass
